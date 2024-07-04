e227c1231f7c2dc2b5d1c35840bc575ae1a53ac8
UrGFisMid <sammys_0714@icloud.com>
2024-07-03 21:15:52 -0700

    party-types startup

---
+++ src/data.js
@@ -0,0 +1,27 @@
+const parties = [
+    {
+        index: 1,
+        status: '#e803fc',
+        name: 'Invite Only'
+    },
+
+    {
+        index: 2,
+        status: '#1cfc03',
+        name: 'Alcohol And Drugs'
+    },
+
+    {
+        index: 3,
+        status: '#03fcf8',
+        name: 'Pool Party'
+    },
+
+    {
+        index: 4,
+        status: '#f0fc03',
+        name: 'Safe Party'
+    }
+];
+
+export default parties;
