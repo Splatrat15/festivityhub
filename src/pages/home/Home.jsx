2ce50bb825f6ef1daa410c8b43d7483f6622ce9f
Splatrat15 <robbie.thiel10@gmail.com>
2024-07-06 19:03:18 -0700

    Added button and fixed partyList

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import './home.css';
 import PartyList from '../../components/partyList/PartyList';
+import AddBtn from '../../components/addBtn/AddBtn'
 
 const Home = () => {
     return(
         <div className="home__container">
             <PartyList/>
+            <AddBtn/>
         </div>
     )
 }
